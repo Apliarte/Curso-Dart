5bebbb3890a50e289d32eb985b05191354fbe2e8
erbolamm <erbolamm@gmail.com>
2024-05-29 22:41:29 +0000

    dia uno

---
+++ 01_variables/tipos_de_variables.dart
@@ -0,0 +1,27 @@
+
+var numero =9;
+var numDecimal=0.9;
+var texto= 'hola';
+var siONo=true;
+var lista=['lunes','martes', 'miercoles', 'jueves', 'viernes'];
+var mapaSemanal ={
+  '1':'lunes',
+  '2':'martes',
+  '3':'miercoles',
+  '4':'jueves',
+  '5':'viernes'
+};
+Object name= 6;
+void printInts(List  a)  {
+  
+  print(a);
+  
+  }
+var numeros=[1,2,3,4,5,6,7,8,9,10];
+void main() {
+  
+
+
+  
+}
+
