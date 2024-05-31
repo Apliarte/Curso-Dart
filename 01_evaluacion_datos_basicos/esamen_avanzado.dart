be0281712510348996382aa8c45b2becf62ec37e
erbolamm <erbolamm@gmail.com>
2024-05-31 08:22:43 +0000

    pasar a android studio

---
+++ 01_evaluacion_datos_basicos/esamen_avanzado.dart
@@ -0,0 +1,25 @@
+/*
+Entendido, aquí tienes un examen más desafiante sobre el apartado de Variables y Tipos de Datos en Dart:
+
+Examen de Variables y Tipos de Datos en Dart
+
+Declaración de Variables Avanzada:
+
+Declara una variable numeroMisterioso de tipo dynamic y asígnale el valor 42. Luego, imprime en la consola el resultado de sumar numeroMisterioso con el número 10.
+
+Tipo de Dato Dinámico:
+
+Utilizando el tipo de dato var, declara una variable datoMisterioso y asígnale un valor de tipo List que contenga elementos de tipo int y String. Luego, imprime en la consola el segundo elemento de la lista.
+
+Tipos de Datos Personalizados:
+
+Define una clase Direccion que tenga propiedades calle de tipo String y numero de tipo int. Luego, crea una variable miDireccion de tipo Direccion y asígnale los valores de tu calle y número. Finalmente, imprime en la consola la dirección completa en una sola línea.
+
+Tipo de Dato Bool:
+
+Crea una variable esMayorDeEdad de tipo bool y asígnale el valor true si tienes más de 18 años, de lo contrario, asigna false. Luego, imprime en la consola un mensaje que indique si eres mayor de edad o no, basado en el valor de esMayorDeEdad.
+
+Recuerda que este examen evaluará tus habilidades para trabajar con diferentes tipos de datos y variables en Dart. ¡Buena suerte!
+https://chatgpt.com/share/9d1a38ba-d151-41ac-811f-4f4659486178
+
+*/
---
+++ 01_evaluacion_datos_basicos/examen_basico.dart
@@ -0,0 +1,63 @@
+/*1. // ¿Cómo se declara una variable entera en Dart? Proporciona un ejemplo.
+
+int ejemplo =4;
+
+// Declara una variable de tipo double llamada altura y asígnale el valor 1.75. Luego, imprime el valor de altura.
+var nombddfsfssfre='8';
+
+
+double altura=1.75;
+main(){
+
+  print(altura);
+}
+*/
+//¿Cuál es la diferencia entre var, final y const en Dart?
+
+/*
+var se puede cambiar el valor pero no el tipado despues de asignar el valor
+const se usa cuando se sabe el valor en tiepon de compilacion
+final se usa cuando no se sabe el valor en tiempo de compilacion 
+
+
+ @noukey dice: 
+ var es una variable, 
+   por lo que podemos modificar su valor, 
+ const es una constante, 
+   por lo que conocemos su valor de antemano y nunca va a cambiar, 
+
+ final es una variable que solo se puede asignar una vez, es decir, 
+  la primera vez que asignemos su valor se convertirá en una costante.
+*/
+
+/*
+Declara una variable bool llamada esEstudiante 
+y asígnale el valor true. Luego, imprime el valor de esEstudiante.
+*/
+
+/*
+bool? esEstudiante;
+
+void main(List<String> args) {
+  esEstudiante=true;
+  print(esEstudiante);
+}*/
+
+
+// 5
+double? decimal;
+
+void main(List<String> args) {
+ 
+decimal= 5.0;
+
+
+
+}
+
+
+
+
+
+
+
