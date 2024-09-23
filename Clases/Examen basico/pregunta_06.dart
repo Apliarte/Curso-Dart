0edfeb74dd641518bf8b3203ea0e09b0ba309a1b
ApliArte <erbolamm@gmail.com>
2024-09-23 11:31:20 +0200

    23 sep

---
+++ Clases/Examen basico/pregunta_06.dart
@@ -0,0 +1,13 @@
+/*
+Define una clase `Coche` en Dart con las propiedades `marca`, `modelo` y `año`.
+ Incluye un constructor para inicializar estas propiedades*/
+
+main() {}
+
+class Coches {
+  String marca;
+  String modelo;
+  int anio;
+
+  Coches(this.marca, this.modelo, this.anio);
+}
---
+++ Clases/Examen basico/preguntasabiertas.dart
@@ -0,0 +1,29 @@
+/*
+6. **Define una clase `Coche` en Dart con las propiedades `marca`, `modelo` y `año`. Incluye un constructor para inicializar estas propiedades.**
+
+7. **Explica la diferencia entre una clase abstracta y una interfaz en Dart. Proporciona un ejemplo de cada una.**
+
+8. **Escribe una clase `Animal` con un método `hacerSonido()`. Luego, crea una clase `Perro` que herede de `Animal` e implemente el método `hacerSonido()` para que imprima "Guau".**
+
+9. **¿Qué es un método estático en Dart? Escribe un ejemplo de una clase con un método estático.**
+
+10. **Describe cómo se puede utilizar la palabra clave `super` en Dart. Proporciona un ejemplo donde una clase hija llame a un método de la clase padre utilizando `super`.**
+
+---*/
+class Volador {
+  void volar() {
+  }
+}
+
+class Pajaro implements Volador {
+  @override
+  void volar() {
+    print('El pájaro está volando');
+  }
+}
+
+void main() {
+  Volador volador = Volador();
+  Pajaro pajaro = Pajaro();
+  pajaro.volar();
+}
